7dd86229e1b5ccc2a93ccebc5170434a23e9df29
zimizo <ibrakhimka@mail.ru>
2023-09-12 09:56:06 +0500

    Initial Commit

---
+++ WhatToWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToWatch
+//
+//  Created by Ибрахим on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
